74b8ba101ce7e64f55cbf38e64d1e727bc1b4897
Hank Lewis <hank@modernatomic.com>
2025-04-11 17:22:11 +0200

    initial commit

---
+++ demo.c
@@ -0,0 +1,13 @@
+#include <glenv.h>
+#include <GL/glew.h>
+#include <RGFW.h>
+#include <stdio.h>
+
+int main() {
+    RGFW_window* win = RGFW_createWindow("a window", RGFW_RECT(0, 0, 800, 600), RGFW_windowCenter | RGFW_windowNoResize);
+    if(glewInit() != GLEW_OK) {
+        printf("Failed to initialize GLEW\n");
+        return 1;
+    }
+    return 0;
+}
